ec7b51e195e949470aae529f1279eccdc0cb6139
soli1411 <soli1411@gmail.com>
2015-09-13 16:46:39 +0200

    Create Power of large numbers.py

---
+++ Power of large numbers.py
@@ -0,0 +1,5 @@
+t=(int)(raw_input())
+for i in range(0,t):
+    a,b=(raw_input().split())
+    print(pow((int)(a),(int)(b),1000000007))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
