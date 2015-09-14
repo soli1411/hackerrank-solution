31f583dac0ff85f7c9f0e3d7f83623833851c53b
soli1411 <soli1411@gmail.com>
2015-09-14 14:18:05 +0200

    Create Akhil and GF solution.cpp

---
+++ Akhil and GF solution.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+long long mod_mul(long long x, long long y, long long n)
+{if (!x) 
+    return 0;
+ return (((x&1)*y)%n+(mod_mul(x>>1,y,n)<<1)%n)%n;
+}
+
+long long ModExp(long long b,long long n)
+{long long c=1, d=10;
+ while (b)
+	{if (b&1) 
+        c=mod_mul(c,d,n);
+	d=mod_mul(d,d,n);
+	b>>=1;
+	}
+	return c;
+}
+
+int main()
+{int t;
+ cin>>t;
+ while (t--)
+    {long long n,m;
+	 cin>>n>>m;
+	 long long tmp=ModExp(n,9*m);
+	 tmp--;
+	 if (tmp<0) tmp+=9*m;
+	 tmp/=9;
+	 cout<<tmp<<endl;
+	}
+	return 0;
+}
