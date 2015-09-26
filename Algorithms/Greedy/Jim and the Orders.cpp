c1231d44ab37082c6ea22b1567a9b1ba1989a537
soli1411 <soli1411@gmail.com>
2015-09-26 15:43:47 +0200

    Create Jim and the Orders.cpp

---
+++ Algorithms/Greedy/Jim and the Orders.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N; 
+    cin>>N;
+    vector<pair<int,int>> orders;
+    int arrival, time;
+    for(int i=0;i<N;i++)
+        {cin>>arrival>>time;
+         orders.push_back(make_pair(arrival+time,i));}
+    sort(orders.begin(),orders.end());
+    for(int i=0;i<N;i++)
+        cout<<orders[i].second+1<<" ";
+    return 0;
+}
