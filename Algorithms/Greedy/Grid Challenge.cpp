e3900d332819a6458c043ba2342800baa2642352
soli1411 <soli1411@gmail.com>
2015-09-26 15:43:04 +0200

    Create Grid Challenge.cpp

---
+++ Algorithms/Greedy/Grid Challenge.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{int t;
+ cin>>t;
+ while (t--)
+     {int n;cin>>n;
+      vector <string> G;
+      string str;
+      bool f=true;
+      for (int i=0;i<n;i++)
+        {cin>>str;
+         sort(str.begin(), str.end());
+         G.push_back(str);
+        }
+      for (int j=0;j<n;j++)
+          for (int i=0;i<n-1;i++)
+            if (G[i][j]>G[i+1][j])
+                {f=0;}
+      if (f)
+          cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
+     }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
