80efdf407f17129196addeac6c2bc6d004932cf6
soli1411 <soli1411@gmail.com>
2015-09-26 15:42:17 +0200

    Create Mark and Toys.cpp

---
+++ Algorithms/Greedy/Mark and Toys.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() 
+{
+    long long n, k; cin >> n >> k;
+    vector<int> prices;
+    for(int i = 0; i < n; i++)
+    {
+        int p; cin >> p;
+        prices.push_back(p);
+    }
+    
+    int answer = 0;
+    
+    // Write the code for computing the final answer using n,k,prices
+    int sum=0;
+    sort(prices.begin(),prices.end());
+    for (int i=0;i<n;i++)
+     {if (sum+prices[i]<=k)
+            {sum+=prices[i];
+             answer++;}
+        else 
+            break;}
+    cout << answer << endl;
+    
+    return 0;
+}
