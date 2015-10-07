43ee809388288bce7f5d60ec8c6fc04ca0cd0271
soli1411 <soli1411@gmail.com>
2015-10-07 10:56:55 +0200

    Create Flipping bits.cpp

---
+++ Algorithms/Bit-Manipulation/Flipping bits.cpp
@@ -0,0 +1,20 @@
+//Flipping bits solution hackerrank
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{int t;
+ cin>>t;
+ while (t--)
+    {unsigned int tmp;
+     cin>>tmp;
+     cout<<~tmp<<endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
